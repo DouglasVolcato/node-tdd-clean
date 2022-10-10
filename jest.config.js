74723ad5acbb0df1933001a1655386548f9a8d45
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-10 18:40:10 -0300

    feat: ensure SignUpController returns error if no email is provided

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   testEnvironment: "node",
   transform: {
     ".+\\.ts$": "ts-jest",
