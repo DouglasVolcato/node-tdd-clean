03807e4e52426f82fc40c8e733e2ef6ac1b6f408
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-13 17:59:40 -0300

    feat: add dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,7 @@
+import { config } from "dotenv";
+config();
+
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api",
-  port: process.env.PORT || 5050
+  mongoUrl: process.env.MONGO_URL,
+  port: process.env.PORT || 5050,
 };
