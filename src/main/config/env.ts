e9930a0e8133eea09f8c75f1d55db235b17f65be
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-14 02:10:15 -0300

    feat: add jest-mongo url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,6 +2,6 @@ import { config } from "dotenv";
 config();
 
 export default {
-  mongoUrl: process.env.MONGO_URL,
+  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/tdd-clean",
   port: process.env.PORT || 5050,
 };
