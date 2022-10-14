45fb447ae94d6b172eece5ff2bb84ca9c0c88823
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-14 17:07:12 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -45,7 +45,7 @@ export class SignUpController implements Controller {
       });
       return ok(account);
     } catch (err) {
-      return serverError();
+      return serverError(err);
     }
   }
 }
