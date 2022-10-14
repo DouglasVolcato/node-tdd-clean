6398dc15a6d431e5fb4a4560fe7bb8902de21723
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-14 01:45:30 -0300

    feat: add PathIgnorePatterns for test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
+  coveragePathIgnorePatterns: ["index.ts", "-protocols.ts"],
   testEnvironment: "node",
   preset: "@shelf/jest-mongodb",
   transform: {
