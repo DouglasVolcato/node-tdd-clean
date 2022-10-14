ff680e6560c703e8d671654a5218452254e57357
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-14 16:29:09 -0300

    test: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/factories/decorators/log.spec.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from "@/presentation/protocols";
+import { LogControllerDecorator } from "./log";
+
+describe("LogController Decorator", () => {
+  test("Should call controller handle", async () => {
+    class ControllerStub implements Controller {
+      async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse = {
+          statusCode: 200,
+          body: {
+            name: "Douglas",
+            email: "douglasvolcato@gmail.com",
+          },
+        };
+        return new Promise((resolve) => resolve(httpResponse));
+      }
+    }
+    const controllerStub = new ControllerStub();
+    const handleSpy = jest.spyOn(controllerStub, "handle");
+    const sut = new LogControllerDecorator(controllerStub);
+    const httpRequest = {
+      body: {
+        email: "any_email@email.com",
+        name: "any_name",
+        password: "any_password",
+        passwordConfitmation: "any_password",
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+});
