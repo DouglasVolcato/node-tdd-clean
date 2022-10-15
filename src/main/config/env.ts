1b71748206997d9ebcfe4cfe31371b607fbf5004
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-15 11:45:59 -0300

    feat: add name

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,6 +2,6 @@ import { config } from "dotenv";
 config();
 
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/tdd-clean",
+  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/node-tdd-clean",
   port: process.env.PORT || 5050,
 };
