6a441bf643b36beadbb0f0924fcba4e50924882f
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-16 17:59:53 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- src/presentation/helpers/validators/validationComposite.spec.ts
+++ src/presentation/helpers/validators/validationComposite.spec.ts
@@ -2,24 +2,43 @@ import { ValidationComposite } from "./validationComposite";
 import { Validation } from "./validation";
 import { MissingParamError } from "../../../presentation/errors";
 
-const makeValidation = () => {
+const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate(input: any): Error {
-      return new MissingParamError("field");
+      return null;
     }
   }
   return new ValidationStub();
 };
 
-const makeSut = (): ValidationComposite => {
-  const validations: Validation[] = [makeValidation()];
-  return new ValidationComposite(validations);
+interface SutTypes {
+  sut: ValidationComposite;
+  validationStubs: Validation[];
+}
+
+const makeSut = (): SutTypes => {
+  const validationStubs: Validation[] = [makeValidation(), makeValidation()];
+  const sut = new ValidationComposite(validationStubs);
+  return { sut, validationStubs };
 };
 
 describe("ValidationComposite", () => {
   test("Should return an error if any validation fails", () => {
-    const sut = makeSut();
+    const { sut, validationStubs } = makeSut();
+    jest
+      .spyOn(validationStubs[0], "validate")
+      .mockReturnValueOnce(new MissingParamError("field"));
     const error = sut.validate({ field: "any_field" });
     expect(error).toEqual(new MissingParamError("field"));
+  });
+
+  test("Should return the first error if more than one validation fails", () => {
+    const { sut, validationStubs } = makeSut();
+    jest.spyOn(validationStubs[0], "validate").mockReturnValueOnce(new Error());
+    jest
+      .spyOn(validationStubs[1], "validate")
+      .mockReturnValueOnce(new MissingParamError("field"));
+    const error = sut.validate({ field: "any_field" });
+    expect(error).toEqual(new Error());
   });
 });
