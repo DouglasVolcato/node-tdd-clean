4af648c32d114c191f833ee49a1902a3229888c2
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-16 15:21:53 -0300

    refactor: mova password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -2,6 +2,7 @@ import { Validation } from "../../presentation/helpers/validators/validation";
 import { RequiredFieldsValidation } from "../../presentation/helpers/validators/required-field-validation";
 import { ValidationComposite } from "../../presentation/helpers/validators/validationComposite";
 import { makeSignUpValidation } from "./signup-validation";
+import { CompareFieldsValidation } from "../../presentation/helpers/validators/compare-fields-validation";
 
 jest.mock("../../presentation/helpers/validators/validationComposite");
 
@@ -12,6 +13,9 @@ describe("SignUpValidationFactory", () => {
     for (const field of ["name", "email", "password", "passwordConfirmation"]) {
       validations.push(new RequiredFieldsValidation(field));
     }
+    validations.push(
+      new CompareFieldsValidation("password", "passwordConfirmation")
+    );
     expect(ValidationComposite).toHaveBeenCalledWith(validations);
   });
 });
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,11 +1,15 @@
 import { Validation } from "../../presentation/helpers/validators/validation";
 import { RequiredFieldsValidation } from "../../presentation/helpers/validators/required-field-validation";
 import { ValidationComposite } from "../../presentation/helpers/validators/validationComposite";
+import { CompareFieldsValidation } from "../../presentation/helpers/validators/compare-fields-validation";
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = [];
   for (const field of ["name", "email", "password", "passwordConfirmation"]) {
     validations.push(new RequiredFieldsValidation(field));
   }
+  validations.push(
+    new CompareFieldsValidation("password", "passwordConfirmation")
+  );
   return new ValidationComposite(validations);
 };
