2af293e4a516aa568999e32dd24a64ebfa1c63d8
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-28 16:50:26 -0300

    refactor: move DbAuthentication protocols to a single file

---
+++ src/data/usecases/add-account/authentication/db-authentication-protocols.ts
@@ -0,0 +1,6 @@
+export * from "../../../../domain/usecases/authentication";
+export * from "../../../protocols/db/load-account-by-email-repository";
+export * from "../db-add-account-protocols";
+export * from "../../../../data/protocols/criptography/hash-comparer";
+export * from "../../../../data/protocols/criptography/token-generator";
+export * from "../../../../data/protocols/db/update-access-token-repository";
