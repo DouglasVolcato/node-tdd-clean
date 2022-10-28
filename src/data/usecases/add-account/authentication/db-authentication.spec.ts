9d6a64be15087aa43c98d920f736a252f14f7841
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-28 11:11:06 -0300

    test: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/add-account/authentication/db-authentication.spec.ts
+++ src/data/usecases/add-account/authentication/db-authentication.spec.ts
@@ -56,4 +56,11 @@ describe("DbAuthentication UseCase", () => {
     const promise = sut.auth(makeFakeAuthentication());
     await expect(promise).rejects.toThrow();
   });
+
+  test("Should null if LoadAccountByEmailRepository returns null", async () => {
+    const { sut, loadAccountByEmailRepository } = makeSut();
+    jest.spyOn(loadAccountByEmailRepository, "load").mockReturnValue(null);
+    const accessToken = await sut.auth(makeFakeAuthentication());
+    expect(accessToken).toBe(null);
+  });
 });
