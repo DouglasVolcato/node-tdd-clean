d1741748f26dd7d11565e6ee69b8fc34e2171b0e
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-29 21:24:17 -0300

    test: ensure JwtAdapter returns a token on sign success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,5 +1,12 @@
+import { sign } from "crypto";
 import jwt from "jsonwebtoken";
 import { JwtAdapter } from "./jwt-adapter";
+
+jest.mock("jsonwebtoken", () => ({
+  async sign(): Promise<string> {
+    return new Promise((resolve) => resolve("any_token"));
+  },
+}));
 
 const makeSut = () => {
   return new JwtAdapter("secret");
@@ -11,5 +18,11 @@ describe("JWT Adapter", () => {
     const signSpy = jest.spyOn(jwt, "sign");
     await sut.encrypt("any_id");
     expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret");
+  });
+
+  test("Should return a token on sign success", async () => {
+    const sut = makeSut();
+    const accessToken = await sut.encrypt("any_id");
+    expect(accessToken).toBe("any_token");
   });
 });
