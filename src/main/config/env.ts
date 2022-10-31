74e5d47147467b27586f0466bb0c9ade429323c4
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-30 23:43:36 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,4 +4,5 @@ config();
 export default {
   mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/node-tdd-clean",
   port: process.env.PORT || 5050,
+  jwtSecret: process.env.SECRET || "hsd83WHD44kf9W7",
 };
